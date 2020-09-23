0aaccc070daae601ce51a9f68a5866751cfe1402
Rohit Tayal <tayal.rohit14@gmail.com>
2020-09-23 23:53:18 +0530

    made card component

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -36,10 +36,27 @@ export default function card() {
           <span className="lightgrey">| 2 Kms away from bus stand</span>
         </div>
 
-        <div className="famous greenblue midsize">
-          93% Match :<span className="grey midsize"> 2.5kms </span>
-          <span className="lightgrey">from GTB Nagar</span>
+        <div className="famous greenblue">
+          93% Match :<span className="grey"> 2.5kms </span>
+          <span className="lightgrey midsize">from GTB Nagar</span>,
         </div>
+
+        <div className="offer grey">
+          <span className="midsize">Flat </span>Rs
+          <span className="greenblue">2,000 </span>off + upto Rs
+          <span className="greenblue">500 </span> wallet! to avail...{" "}
+          <span className="blue"> LOGIN</span>
+        </div>
+
+        <div className="fees grey">₹6000</div>
+
+        <div className="pricetag">20</div>
+
+        <div className="discountedfee">₹ 5200</div>
+
+        <div className="feecycle grey">Per Semester (3months)</div>
+
+        <div className="amenties greenblue">Free Cancellation . Free Wi-Fi</div>
       </div>
     </div>
   );
